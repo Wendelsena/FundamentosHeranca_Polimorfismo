7d74ac3f714d1429bf8a6a86458a35dd0ed12213
Wendelsena <wendel4019@gmail.com>
2024-02-21 15:44:42 -0300

    Adicionado Exemplo De Polimorfismo

--- HerancaFundamentos/src/application/Program.java
+++ HerancaFundamentos/src/application/Program.java
@@ -46,6 +46,19 @@ public class Program {
 		Account acc7 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
 		acc7.withdraw(200.00);
 		System.out.println(acc7.getBalance());
+		
+		//polimorfismo
+		System.out.println("\nPolimorfismo ----------");
+		Account x = new Account(1020, "Alex", 1000.00); // istanciando 2 contas a x é uma comum a y uma poupança 
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		
+		x.withdraw(50.0); // saque de 50
+		y.withdraw(50.0);
+		
+		System.out.println(x.getBalance()); // mostra o saldo final
+		System.out.println(y.getBalance());
+		
+		
 	}
 
 }
